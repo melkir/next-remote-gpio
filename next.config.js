90ea769f8d78eb315d55c247833ae03547f119cd
melkir <melkir13@gmail.com>
2023-05-29 15:19:16 +0200

    feat: config remote gpio url

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ const nextConfig = {
   rewrites: async () => [
     {
       source: '/gpio/:path((?!another-page$).*)',
-      has: [{ type: 'header', key: 'cf-access-client-id' }],
-      destination: 'http://127.0.0.1:5002/:path*',
+      has: [
+        { type: 'header', key: 'cf-access-client-id' },
+        { type: 'header', key: 'cf-access-client-secret' },
+      ],
+      destination: `${process.env.REMOTE_GPIO_URL}/:path*`,
     },
   ],
 }
