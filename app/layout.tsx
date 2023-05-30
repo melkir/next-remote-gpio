43b8eebb706e780be2663b685ebde1474bf4e09c
melkir <melkir13@gmail.com>
2023-05-31 00:52:24 +0200

    feat: pwa schell

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,40 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Analytics } from '@vercel/analytics/react'
 import { cx } from 'class-variance-authority'
 
+import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Remote Controller',
-  description: 'Control your shutters remotely',
+const APP_NAME = 'Remote Controller'
+const APP_DEFAULT_TITLE = 'Remote Controller'
+const APP_DESCRIPTION = 'Control your shutters remotely'
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: APP_DEFAULT_TITLE,
+  description: APP_DESCRIPTION,
+  themeColor: '#0F172A',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: APP_DEFAULT_TITLE,
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: APP_DEFAULT_TITLE,
+    description: APP_DESCRIPTION,
+  },
 }
 
 const config = {
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => [
@@ -12,4 +17,4 @@ const nextConfig = {
   ],
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
