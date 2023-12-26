90a2c1921e158894e6885747d82190052ac58a7d
melkir <melkir13@gmail.com>
2023-12-26 19:57:42 +0900

    feat: migrate to serwist

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
+const withSerwist = require('@serwist/next').default({
+  cacheOnFrontEndNav: true,
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
   disable: process.env.NODE_ENV === 'development',
 })
 
