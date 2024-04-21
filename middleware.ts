9dfe3973706e43071ad8f0bd7ae537907f6a79f6
melkir <melkir13@gmail.com>
2024-04-21 20:30:01 +0900

    fix: security redirect

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { authMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/api/pusher/auth'])
-
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return // if it's a public route, do nothing
-  auth().protect() // for any other route, require auth
+export default authMiddleware({
+  publicRoutes: ['/api/pusher/auth'],
 })
 
 export const config = {
