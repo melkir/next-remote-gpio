cab209325fe41a0590ef5c618c5c0649f0476e11
melkir <melkir13@gmail.com>
2024-09-25 21:13:56 +0200

    fix(gpio): auth using form url encoded

--- app/api/pusher/auth/route.ts
+++ app/api/pusher/auth/route.ts
@@ -3,7 +3,18 @@ import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   const form = await req.formData()
-  const res = await gpio('pusher/auth', { method: 'POST', body: form })
+  
+  // Convert FormData to URLSearchParams
+  const urlEncodedData = new URLSearchParams()
+  for (const [key, value] of form.entries()) {
+    urlEncodedData.append(key, value.toString())
+  }
+
+  const res = await gpio('pusher/auth', {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    method: 'POST',
+    body: urlEncodedData.toString()
+  })
 
   const data = await res.json()
   return NextResponse.json(data)
